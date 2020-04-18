545413b5ee0658a4cd506b9bd65ae71025904179
xyfka <lukasz.maz@gmail.com>
2020-04-18 16:29:23 +0100

    v3 seeds added

--- v3/app.js
+++ v3/app.js
@@ -1,37 +1,16 @@
 var express         = require("express"),
     app             = express(),
     bodyParser      = require("body-parser"),
-    mongoose        = require("mongoose");
+    mongoose        = require("mongoose"),
+    Campground      = require("./models/campground"),
+    seedDB          = require("./seeds");
+
 
 mongoose.set('useUnifiedTopology', true);
 mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
-
-// Schema Setup
-
-var campgroundSchema = new mongoose.Schema({
-        name: String,
-        image: String,
-        description: String
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
-
-// Campground.create(
-//     {
-//         name: "Zakopane", 
-//         image: "https://images.unsplash.com/photo-1559760283-b522593aa0b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=449&q=80",   
-//         description: "This is a silly place!"
-//     }, (err, campground) => {
-//         if(err){
-//             console.log("We have an error!");
-//             console.log(err);
-//         } else {
-//             console.log("Campground created!");
-//             console.log(campground);
-//         }
-//     });
+seedDB();
 
 
 app.get("/", (req, res) => {
@@ -69,10 +48,10 @@ app.get("/campgrounds/new", (req, res) => {
     res.render("new");
 });
 
+// --- SHOW route ---
 
-app.get("/campgrounds/:id", (req, res) => {
-    
-    Campground.findById(req.params.id, (err, foundCampground) => {
+app.get("/campgrounds/:id", (req, res) => {   
+    Campground.findById(req.params.id).populate("comments").exec((err, foundCampground) => {
         if(err){
             console.log(err);
         } else {
